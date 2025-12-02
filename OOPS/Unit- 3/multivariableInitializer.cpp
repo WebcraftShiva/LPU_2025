edd02b57f240d21ce244b33ed865ebc3061c5ced
= <shivam.webcraft@gmail.com>
2025-12-02 15:22:47 +0530

    multi variable initializer list

---
+++ OOPS/Unit- 3/multivariableInitializer.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+class Demo
+{
+
+    int x;
+    float y;
+
+public:
+    Demo(int a, float b) : x(a), y(b)
+    {
+    }
+    void show()
+    {
+        cout << "X: " << x << endl;
+        cout << "Y: " << y << endl;
+    }
+};
+
+int main()
+{
+
+    Demo obj(10, 5.5);
+    obj.show();
+    return 0;
+}
